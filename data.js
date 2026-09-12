462caefe05c5beb532cb69347e7eafbd6e8804ef
JOMVIZ <mariaclaudiapetit@gmail.com>
2026-09-12 17:26:45 -0300

    Act20260911

--- data.js
+++ data.js
@@ -1,42 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-entrada",
-      "name": "Entrada",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "yaw": 0.3526277468315122,
-        "pitch": -0.00250145155078485,
-        "fov": 1.3237681912121593
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.5147169246682264,
-          "pitch": 0.17261692324976252,
-          "rotation": 0,
-          "target": "1-estar-comedor"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-estar-comedor",
-      "name": "Estar Comedor",
+      "id": "0-1-vereda",
+      "name": "1 Vereda",
       "levels": [
         {
           "tileSize": 256,
@@ -60,29 +26,17 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.7300038089046357,
-          "pitch": 0.4482157570601508,
+          "yaw": 0.22317394579210337,
+          "pitch": 0.1936236682986454,
           "rotation": 0,
-          "target": "0-entrada"
-        },
-        {
-          "yaw": -0.36564232471055647,
-          "pitch": 0.09955284624606442,
-          "rotation": 0,
-          "target": "3-hall-pa"
-        },
-        {
-          "yaw": 1.3804346832125312,
-          "pitch": 0.4682003583438039,
-          "rotation": 0,
-          "target": "2-patio"
+          "target": "1-2-estar-comedor"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-patio",
-      "name": "Patio",
+      "id": "1-2-estar-comedor",
+      "name": "2 Estar Comedor",
       "levels": [
         {
           "tileSize": 256,
@@ -106,17 +60,29 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.6664527800383588,
-          "pitch": 0.3568157111413779,
+          "yaw": -1.4488167343019995,
+          "pitch": 0.26417339876018175,
           "rotation": 0,
-          "target": "1-estar-comedor"
+          "target": "6-7-patio"
+        },
+        {
+          "yaw": 2.011746857040942,
+          "pitch": 0.3235016578570278,
+          "rotation": 0,
+          "target": "0-1-vereda"
+        },
+        {
+          "yaw": 2.7607142987661675,
+          "pitch": 0.4787428537932499,
+          "rotation": 0,
+          "target": "2-3-hall-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3-hall-pa",
-      "name": "Hall PA",
+      "id": "2-3-hall-pa",
+      "name": "3 Hall PA",
       "levels": [
         {
           "tileSize": 256,
@@ -140,35 +106,35 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.4610078644183808,
-          "pitch": 0.36549340847792244,
+          "yaw": -1.1181330429606327,
+          "pitch": 0.42349207254286014,
           "rotation": 0,
-          "target": "5-dormitorio1"
+          "target": "5-6-dormitorio-3"
         },
         {
-          "yaw": -1.139589521403824,
-          "pitch": 0.6703568162187139,
+          "yaw": -2.59016124803712,
+          "pitch": 1.003671776920113,
           "rotation": 0,
-          "target": "6-dormitorio2"
+          "target": "1-2-estar-comedor"
         },
         {
-          "yaw": -2.4646766471670443,
-          "pitch": 0.7533422282263516,
+          "yaw": -1.3806293572060948,
+          "pitch": 0.6410944715995761,
           "rotation": 0,
-          "target": "1-estar-comedor"
+          "target": "4-5-dormitorio-2"
         },
         {
-          "yaw": 2.271306591414696,
-          "pitch": 0.5928348401522001,
+          "yaw": 0.47105542575199344,
+          "pitch": 0.9948499575550684,
           "rotation": 0,
-          "target": "4-dormitorio-principal"
+          "target": "3-4-dormitorio-principal"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "4-dormitorio-principal",
-      "name": "Dormitorio Principal",
+      "id": "3-4-dormitorio-principal",
+      "name": "4 Dormitorio Principal",
       "levels": [
         {
           "tileSize": 256,
@@ -192,17 +158,17 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.633125421994956,
-          "pitch": 0.4570691646804601,
+          "yaw": 2.3769828420006505,
+          "pitch": 0.5099488945755652,
           "rotation": 0,
-          "target": "3-hall-pa"
+          "target": "2-3-hall-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "5-dormitorio1",
-      "name": "Dormitorio1",
+      "id": "4-5-dormitorio-2",
+      "name": "5 Dormitorio 2",
       "levels": [
         {
           "tileSize": 256,
@@ -226,17 +192,17 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.5208823904708577,
-          "pitch": 0.5259791683853763,
+          "yaw": 2.8331620709817518,
+          "pitch": 0.9859279222003554,
           "rotation": 0,
-          "target": "3-hall-pa"
+          "target": "2-3-hall-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "6-dormitorio2",
-      "name": "Dormitorio2",
+      "id": "5-6-dormitorio-3",
+      "name": "6 Dormitorio 3",
       "levels": [
         {
           "tileSize": 256,
@@ -260,10 +226,44 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.7686908006775148,
-          "pitch": 0.5665156970680005,
+          "yaw": -2.1838191899914197,
+          "pitch": 0.9222772039128078,
           "rotation": 0,
-          "target": "3-hall-pa"
+          "target": "2-3-hall-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-7-patio",
+      "name": "7 Patio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2646451224679307,
+          "pitch": 0.25050529120552767,
+          "rotation": 0,
+          "target": "1-2-estar-comedor"
         }
       ],
       "infoHotspots": []
@@ -272,8 +272,8 @@ var APP_DATA = {
   "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": false,
+    "autorotateEnabled": true,
     "fullscreenButton": true,
-    "viewControlButtons": true
+    "viewControlButtons": false
   }
 };
